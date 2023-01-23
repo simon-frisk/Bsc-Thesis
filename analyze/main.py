61c00c466fb978fc05ebd06cac5d9dd89f679080
Simon Frisk <simon@simonfrisk.com>
2023-01-23 16:13:08 +0100

    Change file names to be windows compatible

--- analyze/main.py
+++ analyze/main.py
@@ -1,4 +1,4 @@
 import dependency_tree.serialize as serialize
 
-react_dep_tree = serialize.load("../serialized/@tensorflow:tfjs:4.2.0")
+react_dep_tree = serialize.load("../serialized/@tensorflow.tfjs.4.2.0")
 react_dep_tree.print()
