d387510241689c3d8eed42d6813d40e2dcb9a941
Simon Frisk <simon@simonfrisk.com>
2023-01-23 16:11:10 +0100

    Change file names to be windows compatible

--- dependency_tree/serialize.py
+++ dependency_tree/serialize.py
@@ -2,7 +2,7 @@ import pickle
 
 
 def serialize(dependency_node):
-    name = "{}:{}".format(dependency_node.name.replace("/",":"), dependency_node.version)
+    name = "{}.{}".format(dependency_node.name.replace("/","."), dependency_node.version)
     with open("../serialized/{}".format(name), "wb") as outfile:
         pickle.dump(dependency_node, outfile)
 
