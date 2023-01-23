52479fa53e30d2a01320d1ba31422b1d960f8056
folke-bruno <folkebsse@gmail.com>
2023-01-23 16:16:23 +0100

    rename

--- dependency_tree/serialize.py
+++ dependency_tree/serialize.py
@@ -3,7 +3,7 @@ import pickle
 
 def serialize(dependency_node):
     name = "{}.{}".format(dependency_node.name.replace("/","."), dependency_node.version)
-    with open("../serialized/{}".format(name), "wb") as outfile:
+    with open("../serialized/{}.txt".format(name), "wb") as outfile:
         pickle.dump(dependency_node, outfile)
 
 
