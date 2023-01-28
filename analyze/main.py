374061fe5d5d1eaafae8898092991a9f4ca0d28e
Simon Frisk <simon@simonfrisk.com>
2023-01-28 14:02:34 +0100

    Fix problem with certain version specs, implement function to print all versions in a package, and prepare stored files for 4 packages

--- analyze/main.py
+++ analyze/main.py
@@ -1,4 +1,16 @@
 import dependency_tree.serialize as serialize
+import os
 
-react_dep_tree = serialize.load("../serialized/cloudinary.0.0.1.txt")
-react_dep_tree.print()
+
+def print_all_versions(package):
+    directory_path = os.path.join('../serialized', package)
+    files = os.listdir(directory_path)
+    files.sort()
+    for file_name in files:
+        dep_tree = serialize.load(os.path.join(directory_path, file_name))
+        print(f"\nDependency tree for {package}: {file_name}")
+        dep_tree.print()
+
+
+if __name__ == '__main__':
+    print_all_versions('tfjs')
--- api/javascript.py
+++ api/javascript.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import semantic_version
 from dependency_tree.dependency_node import DependencyNode
 from dependency_tree.serialize import serialize
@@ -6,23 +7,37 @@ from dependency_tree.serialize import serialize
 NPM_REGISTRY_API_URL = 'https://registry.npmjs.org/{}'
 
 
-def select_version(semver_spec, versions):
+def select_version(semver_spec, versions, dist_tags):
     """Select the matching version from versions list, using semverspec as matching scheme"""
 
-    # TODO: Handle latest and next
-    if semver_spec == 'latest' or semver_spec == 'next':
-        semver_spec = '*'
+    # Handle special dist tags
+    if semver_spec == 'latest':
+        semver_spec = dist_tags['latest']
+    elif semver_spec == 'next':
+        semver_spec = dist_tags['next']
+    elif semver_spec == 'experimental':
+        semver_spec = dist_tags['experimental']
+    elif semver_spec == 'beta':
+        semver_spec = dist_tags['beta']
+    elif semver_spec == 'rc':
+        semver_spec = dist_tags['rc']
 
-    #Add Regex(?) for <= x <
+    # Remove certain whitespace that causes semver library to crash
+    semver_spec = semver_spec.replace('>= ', '>=').replace('< ', '<')
 
     spec = semantic_version.NpmSpec(semver_spec)
 
-    for version in reversed(versions):
-        semver = semantic_version.Version(version)
-        if semver in spec:
-            return version
+    # Find all matching version and select the highest version
+    best = None
+    semver_objects = map(semantic_version.Version, versions)
+    for version in spec.filter(semver_objects):
+        if not best or version > best:
+            best = version
 
-    raise "No matching version was found"
+    if best:
+        return str(best)
+    else:
+        raise "No matching version was found"
 
 
 def fetch_dependency_from_npm_registry(package, parent_semver):
@@ -36,8 +51,7 @@ def fetch_dependency_from_npm_registry(package, parent_semver):
     package = response.json()
 
     # Select the correct version of the package
-    version = select_version(parent_semver, list(package['versions'].keys()))
-
+    version = select_version(parent_semver, list(package['versions'].keys()), package['dist-tags'])
     version_data = package['versions'][version]
 
     # Create DependencyNode object
@@ -69,17 +83,20 @@ def build_tree_from_npm_registry(package, version):
     build_nested_dependencies(root_node, root_dependencies)
     return root_node
 
-
-# Create and save dependency tree for some package
-if __name__ == "__main__":
-    #versions =["4.18.2", "5.0.0-beta.1", "3.21.2", "4.0.0", "0.14.1", "1.0.0"] #Versions for Express
-    #versions = ["1.33.0", "1.11.0", "1.1.0", "1.0.1", "0.2.7", "0.0.1"]
-    #versions = ["0.0.1", "0.1.2", "0.14.8", "15.0.0", "15.7.0", "16.0.0-alpha", "17.0.2"]
-
-    package = "react"
+def build_multiple_versions(package, versions):
     for version in versions:
         node = build_tree_from_npm_registry(package, version)
         node.print()
         serialize(node)
+
+# Create and save dependency tree for some package
+if __name__ == "__main__":
+    #tf_versions = ['0.0.1', '0.15.3', '1.7.4', '2.8.5', '3.21.0', '4.2.0']
+    #express_versions =["4.18.2", "5.0.0-beta.1", "3.21.2", "4.0.0", "0.14.1", "1.0.0"]
+    #react_versions = ["0.0.1", "0.1.2", "0.14.8", "15.0.0", "15.7.0", "16.0.0-alpha", "17.0.2"]
+    cloudainary_versions = ["1.33.0", "1.11.0", "1.1.0", "1.0.1", "0.2.7", "0.0.1"]
+    build_multiple_versions("cloudinary", cloudainary_versions)
+
+
 
 
