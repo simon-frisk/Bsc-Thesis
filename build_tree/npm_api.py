c32d0cb7a66ee174d3639fa2786e02c66c43077f
Simon Frisk <simon@simonfrisk.com>
2023-02-12 11:33:27 +0100

    Work on package loading

--- build_tree/npm_api.py
+++ build_tree/npm_api.py
@@ -1,12 +1,12 @@
 import requests
 import re
-import os
 import semantic_version
 from dependency_tree.dependency_node import DependencyNode
 from dependency_tree.serialize import serialize
 
 NPM_REGISTRY_API_URL = 'https://registry.npmjs.org/{}'
 
+package_cache = {}
 
 def select_version(semver_spec, versions, dist_tags):
     """Select the matching version from versions list, using semverspec as matching scheme"""
@@ -38,17 +38,25 @@ def select_version(semver_spec, versions, dist_tags):
     if best:
         return str(best)
     else:
-        raise "No matching version was found"
+        raise Exception(f'No matching version was found: {semver_spec}')
 
 def fetch_dependency_from_npm_registry(package):
     """Get package data from npm registry for a javascript package"""
 
+    # Try get from cache
+    if package in package_cache:
+        return package_cache[package]
+
     # Get data from npm registry
     response = requests.get(NPM_REGISTRY_API_URL.format(package))
     if response.status_code != 200:
-        raise "Failed to fetch package"
-    package = response.json()
-    return package
+        raise Exception("Failed to fetch package")
+    package_json = response.json()
+
+    # Save to cache
+    package_cache[package] = package_json
+
+    return package_json
 
 
 def select_dependency_version_data(package, parent_semver):
@@ -83,6 +91,7 @@ def select_analyze_versions(package):
     versions = package["versions"].keys()
     versions = list(filter(lambda v: re.match("^\d+.\d+.\d+$", v), versions))
     versions.sort()
+    return versions
 
     selected_versions = []
     current_major = -1
@@ -103,23 +112,30 @@ def build_from_npm_registry(package):
     trees = []
 
     for version in versions:
-        root_node, root_dependencies = select_dependency_version_data(root_package, version)
-        build_nested_dependencies(root_node, root_dependencies)
-        trees.append(root_node)
+        try:
+            root_node, root_dependencies = select_dependency_version_data(root_package, version)
+            build_nested_dependencies(root_node, root_dependencies)
+            trees.append(root_node)
+            print(f'Loaded {package} version {version}')
+        except:
+            print(f'Failed to load {package} version {version}')
 
     return trees
 
 
 def load_packages():
     '''Load and save all packages in packages.txt'''
+
+    # Todo : Make run concurrently, non blocking requests in versions and packages
+
     packages_file = open("packages.txt", "r")
     packages = packages_file.read().split("\n")
+    package_list = []
     for package_name in packages:
-        print(f'Loading {package_name}')
+        print(f'Loading {package_name} ---------------------------------')
         tree_list = build_from_npm_registry(package_name)
-        os.mkdir(f'../serialized/{package_name}')
-        for tree in tree_list:
-            serialize(tree)
+        package_list.append(tree_list)
+    serialize(packages, package_list)
 
 
 # Create and save dependency trees
--- dependency_tree/serialize.py
+++ dependency_tree/serialize.py
@@ -1,9 +1,19 @@
 import pickle
+from datetime import datetime
+import os
 
 
-def serialize(dependency_node):
-    with open(f'../serialized/{dependency_node.name}/{dependency_node.version}.txt', "wb") as outfile:
-        pickle.dump(dependency_node, outfile)
+def serialize(package_names, package_list):
+    root_dir = f'../serialized/{datetime.now()}'
+    os.mkdir(root_dir)
+    for i in range(len(package_list)):
+        package_name = package_names[i]
+        package = package_list[i]
+        package_dir = f'{root_dir}/{package_name}'
+        os.mkdir(package_dir)
+        for version in package:
+            with open(f'{package_dir}/{version.version}.txt', "wb") as outfile:
+                pickle.dump(version, outfile)
 
 
 def load(location):
